ba93485ed7e95e3b83bbe57897f10514be3c3afb
Christina Tarango <liwentarango@gmail.com>
2020-06-20 13:43:53 -0700

    book model &schema & mongoose connect

---
+++ models/book.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const googlebooks = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  synopsis: String,
+  image: String,
+  link: String
+});
+
+const Book = mongoose.model("Book", googlebooks);
+
+module.exports = Book;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,6 +12,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 // Define API routes here
 
 // Send every other request to the React app
