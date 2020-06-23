98d6c5c342289244434a0a90566405fe6146e573
Anna Vu <ngavu2712@gmail.com>
2020-06-22 20:13:13 -0700

    adding queries to controller

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -3,7 +3,34 @@ const db = require("../models")
 // Defining methods for the booksControlelr
 
 module.exports = {
+
+    // GET "/api/books"
     findAll: function(req, res) {
-        
+        db.Book
+        .find(req.query)
+        .sort({ date: -1})
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err))
+    },
+    findById: function (req, res) {
+        db.Book
+        .findById (req.params.id)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => status(422).json(err))
+    },
+
+    // POST "api/books"
+    create: function(req, res) {
+        db.Book
+        .create(req.body)
+        .then(dbSaved => res.json(dbSaved))
+    },
+
+    // DELETE "/api/books/:id"
+    remove : function(req, res) {
+        .findById ( { _id: req.params.id })
+        .then( deletedBook => deletedBook.remove())
+        .then( deletedBook => res.json(deletedBook))
+        .catch (err => res.status(422).json(err))
     }
-}
+};
