6cf97a5be8a85861cae127c71d45974b236febed
Anna Vu <ngavu2712@gmail.com>
2020-06-26 16:02:16 -0700

    add class component to result where api is queried and rendered

---
+++ client/src/components/Result/index.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import API from "../../utils/API";
+
+class Results extends Component {
+
+    //Initial state
+    state = {
+        savedBooks: [] //Savedbook is empty 
+    }
+
+    //When we start the web browser, update the state of the savedBooks array
+    componentDidMount () {
+        API.savedBooks()
+        .then(savedBooks => this.setState({ savedBooks : savedBooks}))
+        .catch(err => console.log(err))
+    }
+
+    handleSave = book => {
+
+        if(this.state.savedBooks.map(book => book._id).includes(books._id)) {
+            API.deleteBook(book._id)
+            .then(deleteBook => this.setState({ savedBooks: this.state.savedBooks.filter(book => book._id !== deletedBook._id)}))
+            .catch(err => console.log(err));
+
+        } else {
+
+            API.saveBook(book)
+            .then(savedBook => this.setState({ savedBooks: this.state.savedBooks.concat([savedBook])}))
+            .catch(err => console.log(err))
+        }
+    }
+
+    render () {
+
+        return(
+
+            <div>
+                {!this.props.books.length ? (
+                    <h1 className="text-center">No results to display</h1>
+                ) : (
+                    <div>
+                        {this.props.books.map(result => (
+                            <div className="card mb-3" key={result._id}>
+                                <div className="row">
+                                    <div className="col-md-2">
+                                        <img atl={result.title} className="img-fluid" src={result.image} />
+                                    </div>
+
+                                    <div className="col-md-10">
+                                        <div className="card-body">
+                                            <h5 className="card-title"> {result.title} BY {result.authors}</h5>
+                                            <p className="card-text"> {result.description}</p>
+
+                                            <div>
+                                                <a href= {result.link} className="btn badge-pill btn-outline-dark mt-3" target="_blank" >View</a>
+
+                                                <button 
+                                                    className="btn badge-pill btn-outline-warning mt-3 ml-3" 
+                                                    onClick={ () => this.handleSave(result) }
+                                                >
+                                                    {this.state.savedBooks.map(
+                                                        book => book._id
+                                                    ).includes(result._id) ? "Unsave" : "Save"
+                                                    }
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+        )
+    }
+}
+
+export default Results;
