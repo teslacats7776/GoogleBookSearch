f028e7a2e9becdf68a87918e407cae86ea6d1088
Anna Vu <ngavu2712@gmail.com>
2020-06-26 16:26:55 -0700

    update app.js and set up serviceWorker to unregister

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,32 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+//Import Components
+import Nav from "./components/Nav/Nav"
+import Header from "./components/Header"
+import Wrapper from "./components/Wrapper"
+
+//Import Pages
+import Search from "./pages/searchPage"
+
 import "./App.css";
-import Nav from "./components/Nav";
-// import { List } from "./components/List";
-// import SearchBtn from "./components/Search";
-// import SaveBtn from "./components/Save";
-import searchPage from "./pages/searchPage"
 
-function App () {
-  return(
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path = "/" component = {searchPage} />
-        </Switch>
-      </div>
-    </Router>
-  )
+class App extends Component {
+  render () {
+    return(
+      <Router>
+        <div>
+          <Nav />
+          <Header />
+
+          <Wrapper>
+            <Route exact path = "/" component = {Search} />
+            <Route exact path = "/search" component = {Search} />
+          </Wrapper>
+        </div>
+      </Router>
+    )
+  }
 }
-
-
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Google Book Search</h2> <Nav />
-//         </div>
-//         {/* <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p> */}
-       
-//        <List />   
-//        <SaveBtn />
-//        <SearchBtn />
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
